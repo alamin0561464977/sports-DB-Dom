e4817ea140dbc4fe2c88c461145602bbb121e35e
alamin0561464977 <amin23435678901315@gmail.com>
2022-02-28 05:29:58 +0300

    html css OK

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log('alamin')
+const allPlayers = () => {
+    const searchValue = document.getElementById('search-box').value;
+    const url = (`https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+};
